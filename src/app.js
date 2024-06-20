4b945e9d7eafbdefc4267591949401e536d95eb1
Your Name <you@example.com>
2024-06-20 17:19:08 +0530

    Cloudinary and routes

--- src/app.js
+++ src/app.js
@@ -4,22 +4,26 @@ import cors from "cors";
 
 const app = express();
 
+//middlewares
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
-
 app.use(
   express.json({
     limit: "16kb",
   })
 );
-
 app.use(express.urlencoded({ extended: false, limit: "16kb" }));
-
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter); //http://localhost:8000/api/v1/users/register
 
 export { app };
